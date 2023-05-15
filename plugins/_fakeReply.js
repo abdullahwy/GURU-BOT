c9b8c079639ca4ca084e3d4933046a0dd18625ad
Abdullah <121610160+abdullahwy@users.noreply.github.com>
2023-05-15 08:49:17 +0530

    Update _fakeReply.js

--- plugins/_fakeReply.js
+++ plugins/_fakeReply.js
@@ -5,7 +5,7 @@ let handler = m => m
 handler.all = async function (m) {
 	
 	let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? this.user.jid : m.sender
-	let pp = await this.profilePictureUrl(who, 'image').catch(_ => 'https://i.imgur.com/whjlJSf.jpg')
+	let pp = await this.profilePictureUrl(who, 'image').catch(_ => 'Whats-App-Image-2023-05-15-at-08-12-45.jpg')
 	
 	//reply link wa
    global.rpl = { contextInfo: { externalAdReply: { mediaUrl: dygp, mediaType: 'VIDEO', description: 'support group', title: packname, body: 'support group', thumbnailUrl: pp, sourceUrl: dygp }}} 
